60079ae6e23904571bd33b92fa8e069c22d21fc3
João Vitor <jvitorlg26@gmail.com>
2025-11-16 18:14:33 -0300

    rio

--- Assets/Scripts/Scenario/River/LogSpawner.cs
+++ Assets/Scripts/Scenario/River/LogSpawner.cs
@@ -9,27 +9,47 @@ public class LogSpawner : MonoBehaviour
     public Vector3 baseMoveDirection = Vector3.right;
     public float moveSpeed = 6f;
     public float spawnInterval = 5f;
+    public float initialDelay = 0f;
     public int maxLogs = 5; 
 
     private float spawnTimer = 4f;
     private int spawnedLogsCount = 0;
     private bool canSpawn = true;
+    private bool hasStarted = false;
 
     private void Update()
     {   
         if (canSpawn && spawnedLogsCount < maxLogs)
         {
             spawnTimer += Time.deltaTime;
+    
+            if (!hasStarted)
+            {
+                if (spawnTimer >= initialDelay)
+                {
+                    hasStarted = true;
+                    spawnTimer = 0f;
+                }
+                return;
+            }
+            
             if (spawnTimer >= spawnInterval)
             {
-                SpawnLog();
+                if (logPrefab.tag == "Log")
+                {
+                    SpawnLog();
+                }
+                else if (logPrefab.tag == "VitoriaRegia")
+                {
+                    SpawnVitoriaRegia();
+                }
+                 
                 spawnTimer = 0f;
                 spawnedLogsCount++;
                 
                 if (spawnedLogsCount >= maxLogs)
                 {
                     canSpawn = false;
-                    Debug.Log("[LogSpawner] Spawnou " + maxLogs + " logs. Parando spawner.");
                 }
             }
         }
@@ -39,9 +59,24 @@ public class LogSpawner : MonoBehaviour
     {
         if (logPrefab != null && spawnPoint != null)
         {
-            Vector3 randomOffset = new Vector3(Random.Range(-8, 8), 0, 0);
+            Vector3 randomOffset = new Vector3(0, 0, 0);
             
             Quaternion logRotation = Quaternion.Euler(-90, 0, 0);
+            GameObject log = Instantiate(logPrefab, spawnPoint.position + randomOffset, logRotation);
+
+            LogMovement logMovement = log.AddComponent<LogMovement>();
+            logMovement.moveDirection = (baseMoveDirection).normalized;
+            logMovement.moveSpeed = moveSpeed;
+        }
+    }
+
+    public void SpawnVitoriaRegia()
+    {
+        if (logPrefab != null && spawnPoint != null)
+        {
+            Vector3 randomOffset = new Vector3(0, 0, 0);
+
+            Quaternion logRotation = Quaternion.Euler(0, 0, 0);
             GameObject log = Instantiate(logPrefab, spawnPoint.position + randomOffset, logRotation);
 
             LogMovement logMovement = log.AddComponent<LogMovement>();
